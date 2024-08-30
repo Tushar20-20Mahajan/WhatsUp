8ac99cc0865a4de225950bbd3924b45d325498a6
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-08-30 17:11:34 +0530

    completed the UI of community Tab Screen

--- WhatsUp/CommunityTabScreen.swift
+++ WhatsUp/CommunityTabScreen.swift
@@ -1,15 +1,50 @@
-//
-//  CommunityTabScreen.swift
-//  WhatsUp
-//
-//  Created by student on 30/08/24.
-//
-
 import SwiftUI
 
 struct CommunityTabScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            ScrollView{
+                VStack(alignment: .leading , spacing: 10){
+                    Image(.communities)
+                    
+                    Group{
+                        Text("Stay connected with a community")
+                            .font(.title2)
+                        
+                        Text("Communities bring members together in topic-based groups. Any community you are added to will appear here.")
+                            .foregroundStyle(.gray)
+                    }
+                    .padding(.horizontal , 5)
+                    
+                    HStack {
+                        Button("See example communities"){ }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                    }
+                    
+                    createNewCommunityButton()
+                    
+                }
+                .navigationTitle("Communities")
+                .padding()
+            }
+        }
+    }
+    
+    private func createNewCommunityButton() -> some View {
+        HStack {
+            Button{
+                
+            }label: {
+                Label("New Community" , systemImage: "plus")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(10)
+                    .background(Color(.systemBlue))
+                    .foregroundColor(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10 , style: .continuous))
+                    .padding()
+            }
+        }
     }
 }
 
--- WhatsUp/MainTabView.swift
+++ WhatsUp/MainTabView.swift
@@ -17,7 +17,7 @@ struct MainTabView: View {
                     Image(systemName: Tab.calls.icon)
                     Text(Tab.calls.title)
                 }
-            placeholderView("Communites")
+            CommunityTabScreen()
                 .tabItem {
                     Image(systemName: Tab.communities.icon)
                     Text(Tab.communities.title)
