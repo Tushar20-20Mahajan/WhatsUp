61bb739ae8f6487660afcbc255b9639537dbbef1
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-08-30 17:08:46 +0530

    UI of community tab screen completed and structured the project

---
+++ WhatsUp/CommunityTabScreen.swift
@@ -0,0 +1,18 @@
+//
+//  CommunityTabScreen.swift
+//  WhatsUp
+//
+//  Created by student on 30/08/24.
+//
+
+import SwiftUI
+
+struct CommunityTabScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommunityTabScreen()
+}
